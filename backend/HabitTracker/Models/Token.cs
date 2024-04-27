b428091c7e14664cd523f082d2f17cf73f605f62
am-katzest <am-katzest@proton.me>
2024-04-27 03:06:28 +0200

    fix token date  & creation

--- backend/HabitTracker/Models/Token.cs
+++ backend/HabitTracker/Models/Token.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
@@ -7,8 +8,10 @@ namespace HabitTracker.Models;
 public class Token
 {
     [StringLength(40)]
-    public required String Id { get; set; } = RandomNumberGenerator.GetHexString(40);
+    public String Id { get; set; } = RandomNumberGenerator.GetHexString(40);
 
+    // https://stackoverflow.com/questions/73693917/net-postgres-ef-core-cannot-write-datetime-with-kind-local-to-postgresql-type/78119521#78119521
+    [Column(TypeName = "timestamp(6)")]
     public required DateTime ExpirationDate { get; set; }
 }
 
