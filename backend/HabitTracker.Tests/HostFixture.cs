c6dcc7d9ab4670c4284475acc1c6cc8a8247c79d
am-katzest <am-katzest@proton.me>
2024-06-06 14:16:49 +0200

    + host test fixture (TestHost)

---
+++ backend/HabitTracker.Tests/HostFixture.cs
@@ -0,0 +1,32 @@
+using HabitTracker.Helpers;
+using HabitTracker.Services;
+using Microsoft.AspNetCore.TestHost;
+
+namespace HabitTracker.Tests;
+
+public class HostFixture : UniqueDatabaseFixture
+{
+    // doing all the setup takes ~10ms
+    public IHost makeHost()
+    {
+        return new HostBuilder()
+            .ConfigureWebHost(webBuilder =>
+            {
+                webBuilder
+                    .UseTestServer()
+                    .ConfigureServices(services =>
+                    {
+
+                        services.AddControllers().AddApplicationPart(typeof(HabitTracker.Models.HabitTrackerContext).Assembly);
+                        services.RegisterServices(MakeContext, new RealClock());
+                    }).Configure(x =>
+                    {
+                        x.UseAuthentication();
+                        x.UseRouting();
+                        x.UseEndpoints(e => e.MapControllers());
+                    });
+            })
+            .Start();
+
+    }
+}
