9ed357af9577425685db17c0fc9aac41371d0a8e
am-katzest <am-katzest@proton.me>
2024-06-26 19:20:07 +0200

    + url helper, fix default webdriver

--- frontend-test/src/frontend_test/setup.clj
+++ frontend-test/src/frontend_test/setup.clj
@@ -6,8 +6,9 @@
 (def ROOT (or (System/getenv "FRONTEND_TEST_URL")
               "http://localhost:8880/"))
 
-(def DRIVER (get {"FIREFOX" e/firefox} (System/getenv "FRONTEND_TEST_WEBDRIVER")
-                 "http://localhost:8280/"))
+(defn url [rest] (str ROOT [rest]))
+
+(def DRIVER (get {"FIREFOX" e/firefox} (System/getenv "FRONTEND_TEST_WEBDRIVER") e/firefox))
 (defn make-driver [] (DRIVER))
 (defmacro with-driver  [name & exprs]
   `(let [~name (e/firefox)]
