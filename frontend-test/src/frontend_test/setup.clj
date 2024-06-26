d9d086822f5ed05f441a6fc8c00b9d3fecd8558f
am-katzest <am-katzest@proton.me>
2024-06-26 19:20:38 +0200

    add helper macro to declutter etaoin tests

--- frontend-test/src/frontend_test/setup.clj
+++ frontend-test/src/frontend_test/setup.clj
@@ -1,6 +1,7 @@
 #!/usr/bin/bb
 (ns frontend-test.setup
   (:require
+   [clojure.walk :refer [postwalk]]
    [etaoin.api :as e]))
 
 (def ROOT (or (System/getenv "FRONTEND_TEST_URL")
@@ -15,3 +16,18 @@
      (try ~@exprs
           (finally
             (e/quit ~name)))))
+
+(defmacro with-driver-no-clutter  [first & forms]
+  (let [to-replace (namespace (symbol first))
+        u (gensym)
+        transform (fn [form] (println form)
+                    (if-not (and (seq? form) (> (count form) 0))
+                      form
+                      (let [[first & rest] form]
+                        (if-not
+                         (and (symbol? first)
+                              (= to-replace (namespace (symbol first))))
+                          form
+                          (concat [first u] rest)))))
+        updated (postwalk transform forms)]
+    `(s/with-driver ~u ~@updated)))
