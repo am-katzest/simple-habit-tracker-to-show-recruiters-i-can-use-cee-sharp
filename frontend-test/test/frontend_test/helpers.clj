05b0be7b443887057f623f4d0d4bca4c7d1d9ab1
am-katzest <am-katzest@proton.me>
2024-06-27 20:50:24 +0200

    add helpers for gui tests

---
+++ frontend-test/test/frontend_test/helpers.clj
@@ -0,0 +1,33 @@
+(ns frontend-test.helpers
+  (:require [frontend-test.setup :as s :refer-macros true]
+            [etaoin.api :as e]
+            [clojure.test :refer [is]]))
+
+(defn tag-selector-factory-factory [x]
+  (fn self
+    ([] {:tag x})
+    ([id] (assoc (self) :data-testid id))
+    ([id enabled] (assoc (self id) :fn/enabled enabled))))
+
+(def btn (tag-selector-factory-factory :button))
+
+(def input (tag-selector-factory-factory :input))
+
+(defn wrap-waiter-into-test [f]
+  (fn [& args]
+    (is (any? (apply f s/*driver* args)) args)))
+
+(def wait-enabled (wrap-waiter-into-test e/wait-enabled))
+(def wait-exists (wrap-waiter-into-test e/wait-exists))
+(def wait-disabled (wrap-waiter-into-test e/wait-disabled))
+
+(defn query [s]
+  (reduce str (drop-while (complement #{\?}) s)))
+
+(def short-wait
+  {:timeout 0.5
+   :interval 0.01})
+(defmacro with-wait [params & exprs]
+  `(binding [e/*wait-timeout* (:timeout ~params)
+             e/*wait-interval* (:interval ~params)]
+     ~@exprs))
