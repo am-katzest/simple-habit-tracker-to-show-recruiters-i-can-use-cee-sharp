9c2c8bb4c509608641d8fb61777382a49adf284e
am-katzest <am-katzest@proton.me>
2024-07-11 13:29:08 +0200

    change Habit model

--- backend/HabitTracker/Models/Habit.cs
+++ backend/HabitTracker/Models/Habit.cs
@@ -7,10 +7,10 @@ namespace HabitTracker.Models;
 public class Habit
 {
     public int Id { get; set; }
-    public required User User { get; set; }
+    public User User { get; set; } = null!;
     public required int UserId { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
-    public required ICollection<Completion> Completions { get; set; }
-    public required ICollection<CompletionType> CompletionTypes { get; set; }
+    public List<Completion> Completions { get; set; } = null!;
+    public List<CompletionType> CompletionTypes { get; set; } = null!;
 }
