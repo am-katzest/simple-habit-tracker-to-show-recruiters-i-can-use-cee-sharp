fbd9b7703d9fdea88572631f24efad8579dda167
am-katzest <am-katzest@proton.me>
2024-07-11 13:29:38 +0200

    + habit DTOs

---
+++ backend/HabitTracker/DTOs/Habit.cs
@@ -0,0 +1,10 @@
+using HabitTracker.DTOs;
+namespace HabitTracker.DTOs.Habit;
+
+public record HabitNameDescription(string Name, string? Description);
+
+public record HabitNameId(string Name, int Id);
+
+public record HabitNameDescriptionId(string Name, int Id, string? Description);
+
+public record IdUser(int Id, User.IdOnly User);
