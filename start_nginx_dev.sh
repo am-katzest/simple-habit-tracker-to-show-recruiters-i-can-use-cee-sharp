50f996d0581a4f10c79f99bf425cc57bd8904f7c
bestestest kitteh <236551@edu.p.lodz.pl>
2024-07-21 13:16:51 +0200

    start ngnix dev detached, fix docker warning

--- start_nginx_dev.sh
+++ start_nginx_dev.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run --network=host  -v=${PWD}/nginx_dev.conf:/etc/nginx/nginx.conf:ro -p 8880:8880  nginx:1.25.5-alpine
+docker run -d --network=host  -v="${PWD}/nginx_dev.conf:/etc/nginx/nginx.conf:ro"  nginx:1.25.5-alpine
