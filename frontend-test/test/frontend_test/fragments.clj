7233a43654e217cad5f0b7006512852210b417c4
bestestest kitteh <236551@edu.p.lodz.pl>
2024-07-24 21:52:50 +0200

    + helpers for robust clicking and filling forms

--- frontend-test/test/frontend_test/fragments.clj
+++ frontend-test/test/frontend_test/fragments.clj
@@ -3,36 +3,43 @@
             [frontend-test.setup :as s :refer-macros true]
             [frontend-test.helpers :refer [btn input wait-enabled wait-disabled wait-exists wait-predicate query] :as h  :refer-macros true]
             [etaoin.api :as e]))
+(defn click [p]
+  (s/use-driver
+   e/go
+   (wait-enabled p)
+   (e/click p)))
+
+(defn fill [p text]
+  (s/use-driver
+   e/go
+   (wait-enabled p)
+   (e/clear p)
+   (e/fill p text)))
 
 (defn goto-login []
   (s/use-driver
    e/go
    (e/go s/ROOT)
-   (wait-exists (btn :login))
-   (e/click (btn :login))
-   (wait-exists (btn :login-button))
-   (is (e/exists? (btn :login-button false)))))
+   (click (btn :login))
+   (wait-exists (btn :login-button false))))
 
 (defn goto-register []
   (s/use-driver
    e/go
    (e/go s/ROOT)
-   (wait-exists (btn :new-account))
-   (e/click (btn :new-account))
-   (wait-exists (btn :register-button))
-   (is (e/exists? (btn :register-button false)))))
+   (click (btn :new-account))
+   (wait-exists (btn :register-button false))))
 
 (defn login-as [username password]
   (s/use-driver
    e/go
    (goto-login)
-   (is (e/exists? (btn :login-button false)))
-   (e/fill (input :login-password) password)
+   (fill (input :login-password) password)
    (wait-disabled (btn :login-button))
-   (e/fill (input :login-username) username)
+   (fill (input :login-username) username)
    (wait-enabled (btn :login-button))
    (e/wait 0.1)
-   (e/click (btn :login-button))
+   (click (btn :login-button))
    (wait-predicate #(or (e/exists? (btn :nav-logout))
                         (e/has-text? "invalid username or password"))) ; placeholder (only english)
    (e/exists? (btn :nav-logout))))
@@ -41,16 +48,15 @@
   (s/use-driver
    e/go
    (goto-register)
-   (e/fill (input :register-username) username)
-   (e/fill (input :register-password) password)
-   (e/fill (input :register-password2) password)
-   (wait-enabled (btn :register-button))
-   (e/click (btn :register-button))))
+   (fill (input :register-username) username)
+   (fill (input :register-password) password)
+   (fill (input :register-password2) password)
+   (click (btn :register-button))))
 
 (defn wait-logged-in []
   (s/use-driver
    e/go
-   (e/wait-exists (btn :nav-logout))))
+   (wait-exists (btn :nav-logout))))
 
 (defn new-user []
   (let [username (h/random-str)
@@ -63,4 +69,4 @@
   (s/use-driver
    e/go
    (e/click (btn nav))
-   (e/wait-exists (assoc (btn nav) :fn/has-class :nav-disabled))))
+   (wait-exists (assoc (btn nav) :fn/has-class :nav-disabled))))
