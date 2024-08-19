ef26b2eafe97644ed0f21fd6dbb82f41bf507d9c
am-katzest <am-katzest@proton.me>
2024-08-19 18:41:07 +0200

    + CompletionTypeRemovalStrategy DTO

--- backend/HabitTracker/DTOs/CompletionType.cs
+++ backend/HabitTracker/DTOs/CompletionType.cs
@@ -7,3 +7,14 @@ public record CompletionTypeData([RegularExpression("^#[0-9a-fA-F]{6}$")][Requir
 public record CompletionTypeDataId(string Color, string Name, string? Description, int Id);
 
 public record CompletionTypeId(int Id, HabitId Habit);
+
+
+public enum ColorReplacementStrategy
+{
+    NeverReplace,
+    AlwaysReplace,
+    ReplaceOnlyIfNotSet
+}
+
+// it would be so much easier if c# supported enums with values
+public record CompletionTypeRemovalStrategy(String? Note, ColorReplacementStrategy ColorStrategy, bool DeleteCompletions);
