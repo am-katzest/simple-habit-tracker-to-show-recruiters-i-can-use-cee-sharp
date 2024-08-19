66a778e226bdf352ad9b68f56ca28fe1e6e81917
am-katzest <am-katzest@proton.me>
2024-08-19 21:00:59 +0200

    add validation to CompletionTypeRemovalStrategy

--- backend/HabitTracker/DTOs/CompletionType.cs
+++ backend/HabitTracker/DTOs/CompletionType.cs
@@ -17,4 +17,14 @@ public enum ColorReplacementStrategy
 }
 
 // it would be so much easier if c# supported enums with values
-public record CompletionTypeRemovalStrategy(String? Note, ColorReplacementStrategy ColorStrategy, bool DeleteCompletions);
+public record CompletionTypeRemovalStrategy(String? Note, ColorReplacementStrategy ColorStrategy, [Required] bool DeleteCompletions) : IValidatableObject
+{
+    IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+    {
+        var otherFieldsNotEmpty = ColorStrategy is not ColorReplacementStrategy.NeverReplace || Note is not null;
+        if (DeleteCompletions && otherFieldsNotEmpty)
+        {
+            yield return new ValidationResult($"when {nameof(DeleteCompletions)} is set other fields should not demand changes");
+        }
+    }
+}
