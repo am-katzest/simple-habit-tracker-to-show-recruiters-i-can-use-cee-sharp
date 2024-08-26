a6b8b31aa558fc3fa2e8a981f6789644ce36a735
am-katzest <am-katzest@proton.me>
2024-08-26 17:00:54 +0200

    fix typo in run-integration-tests.sh

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker-compose up -d --build || exit 2
+docker compose up -d --build || exit 2
 id=$(docker build frontend-test -q) || exit 3
 docker run --network=host -e FRONTEND_TEST_URL="http://localhost:8580/" "$id"
